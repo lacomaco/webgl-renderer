d751592226f1b1fab409a8c86e9496b55cad549c
laco and maco <lacomaco122@gmail.com>
2023-11-08 18:49:32 +0900

    depth buffer 테스트용 Circle 생성
    orthogonal projection 잘 작동하는지 보기 위해서 추가

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,17 +1,13 @@
-import { TextureBox } from "../models/textureBox";
-import { Triangle } from "../models/triangle";
+import { Circle2D } from "../models/circle2D";
 
 export class Renderer {
     canvas = document.createElement('canvas');
     gl = this.canvas.getContext("webgl2");
 
-    triangle: Triangle;
-    box: TextureBox;
+    circles: Circle2D[] = [];
 
     constructor() {
         this.initialize();
-        this.triangle = new Triangle(this.gl);
-        this.box = new TextureBox(this.gl);
     }
 
     initialize(){
@@ -19,15 +15,25 @@ export class Renderer {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         
-        this.gl?.viewport(0,0,this.gl.canvas.width, this.gl.canvas.height);
+        this.gl?.viewport(0,0,this.canvas.width, this.canvas.height);
+
+        if(this.gl){
+            this.circles.push(new Circle2D(
+                this.gl,
+                [0,0,0.1],
+                [1,0,0],
+                0.3
+            ));
+        }
     }
 
     render(){
         if(!this.gl) return;
         this.gl.clearColor(0, 0, 0, 0);
         this.gl.clear(this.gl.COLOR_BUFFER_BIT);
-        this.triangle.drawTriangle();
-        this.box.drawBox();
+        this.circles.forEach((circle)=>{
+            circle.draw();
+        })
     }
 
     keepRerender(){
