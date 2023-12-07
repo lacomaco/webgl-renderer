d5d5d6fec30f158f10a9c1e4a1850cec6c95b07f
laco and maco <lacomaco122@gmail.com>
2023-12-07 14:50:19 +0900

    index 정리 ㅇㅅㅇㅋ~ 안쓰는 코드 다 죽임 ㅇㅅㅇ... ㅇㅅㅇㅋ~

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,16 @@
-import { FBXLoader } from "three/examples/jsm/loaders/fbxLoader.js";
-import * as THREE from "three";
+import { Model } from "./loader/model"
 
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
-);
-camera.position.set(0, 0, 2);
-const render = new THREE.WebGLRenderer();
-render.setSize(window.innerWidth, window.innerHeight);
-render.setClearColor(0xffffff, 1);
-
-document.body.appendChild(render.domElement);
-
-const fbxLoader = new FBXLoader();
-
-fbxLoader.load(
-  "./src/assets/zelda/zeldaPosed001.fbx",
-  (object) => {
-    console.log("fbx", object);
-    object.scale.set(0.005, 0.005, 0.005);
-    scene.add(object);
-    render.render(scene, camera);
-    object.traverse(function (child: any) {
-      if (child.isMesh) {
-        console.log(child.material.map);
-      }
-    });
-  },
-  (progress) => {
-    console.log(progress);
-  },
-  (error) => {
-    console.error(error);
-  },
+// fbx 로드 테스트용도
+const fbxModel = new Model(
+  '',
+  "./src/assets/zelda/zeldaPosed001.fbx"
 );
 
-// assimp 테스트용 코드
+// obj 로드 테스트 용도
+/*
+const objModel = new Model(
+  '',
+  "./src/assets/train/electrictrain.obj",
+  "./src/assets/train/electrictrain.mtl"
+);
+*/
