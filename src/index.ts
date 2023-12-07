6f3610f7edfcbb781e7df90a7a896c5c65dbd38e
laco and maco <lacomaco122@gmail.com>
2023-12-07 16:29:27 +0900

    코드정리

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-import { Model } from "./loader/model";
+import { Renderer } from "./renderer/renderer";
 
-// fbx 로드 테스트용도
-const fbxModel = new Model("", "./src/assets/zelda/zeldaPosed001.fbx");
-
-// obj 로드 테스트 용도
-/*
-const objModel = new Model(
-  '',
-  "./src/assets/train/electrictrain.obj",
-  "./src/assets/train/electrictrain.mtl"
-);
-*/
+const render = new Renderer();
+render.keepRerender();
